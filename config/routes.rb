b307f45bb773c32fd9937aca59b99c9da0393725
chemron <camerontasmith@gmail.com>
2022-02-17 11:54:02 +1100

    some prettying

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # static pages
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
+
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
 end
